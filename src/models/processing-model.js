86d4f99d95431d7bf61b3e95e742952b715a6d6f
mvrzan <mvrzan@icloud.com>
2024-05-08 13:41:15 -0400

    construct the URL outside of the config

--- src/models/processing-model.js
+++ src/models/processing-model.js
@@ -3,10 +3,11 @@ import process from "process";
 
 const processingModel = async (token) => {
   const userId = "003am000001BWl2AAG";
+  const url = `${process.env.SALESFORCE_INSTANCE_URL}/services/data/${process.env.SALESFORCE_API_VERSION}/consent/action/processing?ids=${userId}&mode=cdp`;
 
   const config = {
     method: "GET",
-    url: `${process.env.SALESFORCE_INSTANCE_URL}/services/data/${process.env.SALESFORCE_API_VERSION}/consent/action/processing?ids=${userId}&mode=cdp`,
+    url,
     headers: {
       Authorization: `Bearer ${token}`,
     },
