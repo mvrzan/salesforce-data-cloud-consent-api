508c9309b49a75251d8ba814572626a0b80270ab
mvrzan <mvrzan@icloud.com>
2024-05-08 13:34:38 -0400

    add a dedicated shouldforget model

---
+++ src/controllers/should-forget-consent-api-controller.js
@@ -0,0 +1,28 @@
+import authTokenModel from "../models/auth-token-model.js";
+import shouldForgetModel from "../models/processing-model.js";
+
+const shouldForgetConsentApiController = async (_request, reply) => {
+  try {
+    const token = await authTokenModel();
+    const response = await shouldForgetModel(token);
+
+    reply.statusCode = 200;
+
+    return {
+      message:
+        "The request to the Consent API and shouldForget action was successful.",
+      data: response.data,
+    };
+  } catch (error) {
+    console.error("There was an error with the request:", error);
+    reply.status(500).send(error);
+
+    return {
+      message: "There was an error with the request.",
+      data: error,
+      status: 500,
+    };
+  }
+};
+
+export default shouldForgetConsentApiController;
