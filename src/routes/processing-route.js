72b605c3184389b7eb0acda7af1e7fd3724a79d1
mvrzan <mvrzan@icloud.com>
2024-05-10 10:08:03 -0400

    add a param

--- src/routes/processing-route.js
+++ src/routes/processing-route.js
@@ -3,7 +3,7 @@ import fetchProcessing from "../controllers/fetch-processing.js";
 const processingRoute = (fastify, _options, done) => {
   fastify.route({
     method: "GET",
-    url: "/processing",
+    url: "/processing/:id",
     handler: fetchProcessing,
   });
 
