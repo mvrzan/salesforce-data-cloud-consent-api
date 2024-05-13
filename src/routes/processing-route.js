71c1c9ddb082f908127c6a1fd2f56b43788e3bff
mvrzan <mvrzan@icloud.com>
2024-05-13 11:06:32 -0400

    add a route for PATCH request

--- src/routes/processing-route.js
+++ src/routes/processing-route.js
@@ -1,10 +1,17 @@
 import fetchProcessing from "../controllers/fetch-processing.js";
+import updateProcessing from "../controllers/update-processing.js";
 
 const processingRoute = (fastify, _options, done) => {
   fastify.route({
     method: "GET",
     url: "/processing/:id",
     handler: fetchProcessing,
+  });
+
+  fastify.route({
+    method: "PATCH",
+    url: "/processing/:id",
+    handler: updateProcessing,
   });
 
   done();
