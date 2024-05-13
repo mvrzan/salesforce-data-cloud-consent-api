bc92c134bb2ebfbd653f843f7bdb3b2ed1dd2661
mvrzan <mvrzan@icloud.com>
2024-05-13 10:20:53 -0400

    account for the initial get request

--- src/utils/get-processing-status.js
+++ src/utils/get-processing-status.js
@@ -2,22 +2,41 @@ import axios from "axios";
 import process from "process";
 
 const getProcessingStatus = async (token, userId) => {
-  const url = `${process.env.SALESFORCE_INSTANCE_URL}/services/data/${process.env.SALESFORCE_API_VERSION}/consent/action/processing?ids=${userId}&mode=cdp`;
+  const getUrl = `${process.env.SALESFORCE_INSTANCE_URL}/services/data/${process.env.SALESFORCE_API_VERSION}/consent/action/processing?ids=${userId}&mode=cdp`;
+  const patchUrl = `${process.env.SALESFORCE_INSTANCE_URL}/services/data/${process.env.SALESFORCE_API_VERSION}/consent/action/processing?ids=${userId}&mode=cdp&status=optout`;
 
-  const config = {
+  const getConfig = {
     method: "GET",
-    url,
+    url: getUrl,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const patchConfig = {
+    method: "PATCH",
+    url: patchUrl,
     headers: {
       Authorization: `Bearer ${token}`,
     },
   };
 
   try {
-    const response = await axios.request(config);
+    const response = await axios.request(getConfig);
     console.log(
       "response from CONSENT API and processing action:",
       response.data
     );
+
+    if (
+      response.data.results?.result === "Failure" &&
+      response.data.results?.errorMessage.includes("INVALID_ID_FIELD")
+    ) {
+      const patchResponse = await axios.request(patchConfig);
+      console.log("patchResponse:", patchResponse.data);
+
+      return patchResponse;
+    }
 
     return response;
   } catch (error) {
