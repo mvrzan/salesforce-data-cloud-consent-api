92eeb917f05cadbc1256d84dd85df96af19001f3
mvrzan <mvrzan@icloud.com>
2024-05-22 14:29:49 -0400

    update the component

--- src/pages/ConfigurationScreen.jsx
+++ src/pages/ConfigurationScreen.jsx
@@ -1,7 +1,56 @@
 import { Heading } from "@chakra-ui/react";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Card, CardHeader, CardBody, CardFooter } from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
+import { SettingsIcon } from "@chakra-ui/icons";
+import { HStack } from "@chakra-ui/react";
+
+import ApiCredentials from "../components/ApiCredentials";
 
 const ConfigurationScreen = () => {
-  return <Heading>Hello from the server!</Heading>;
+  return (
+    <ChakraProvider>
+      <Flex justifyContent="center" alignItems="center" marginTop="20px">
+        <Box w="50%" margin="20px">
+          <Card size="lg" h="auto">
+            <CardHeader>
+              <HStack spacing="10px">
+                <SettingsIcon boxSize="6" />
+                <Heading size="lg">Configuration</Heading>
+              </HStack>
+            </CardHeader>
+            <CardBody>
+              <Tabs isFitted>
+                <TabList>
+                  <Tab>API credentials</Tab>
+                  <Tab>Should Forget</Tab>
+                  <Tab>Processing</Tab>
+                  <Tab>Portability</Tab>
+                </TabList>
+                <TabPanels>
+                  <TabPanel>
+                    <ApiCredentials />
+                  </TabPanel>
+                  <TabPanel>
+                    <p>two!</p>
+                  </TabPanel>
+                  <TabPanel>
+                    <p>three!</p>
+                  </TabPanel>
+                  <TabPanel>
+                    <p>four!</p>
+                  </TabPanel>
+                </TabPanels>
+              </Tabs>
+            </CardBody>
+          </Card>
+        </Box>
+      </Flex>
+    </ChakraProvider>
+  );
 };
 
 export default ConfigurationScreen;
