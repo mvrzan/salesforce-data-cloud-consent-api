58e4190112d4eb3247e999ebb887161673cde1d0
mvrzan <mvrzan@icloud.com>
2024-05-23 14:42:45 -0400

    add new portability route

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import Fastify from "fastify";
 import process from "process";
 import nextJs from "@fastify/nextjs";
 import processingRoute from "./routes/processing-route.js";
+import portabilityRoute from "./routes/portability-route.js";
 import shouldForgetRoute from "./routes/should-forget-route.js";
 
 dotenv.config();
@@ -10,6 +11,7 @@ const fastify = Fastify({ logger: true });
 
 fastify.register(processingRoute, { prefix: "/api/v1" });
 fastify.register(shouldForgetRoute, { prefix: "/api/v1" });
+fastify.register(portabilityRoute, { prefix: "/api/v1" });
 fastify.register(nextJs).after(() => {
   fastify.next("/ConfigurationScreen");
 });
