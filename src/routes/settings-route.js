23451c06068e8d3be1aa5a715026e9f9423345d4
mvrzan <mvrzan@icloud.com>
2024-05-28 09:40:23 -0400

    remove unused route

--- src/routes/settings-route.js
+++ src/routes/settings-route.js
@@ -1,13 +1,6 @@
 import updateUserDb from "../controllers/update-user-db.js";
-import updateSettings from "../controllers/update-settings.js";
 
 const settingsRoute = (fastify, _options, done) => {
-  fastify.route({
-    method: "POST",
-    url: "/configuration",
-    handler: updateSettings,
-  });
-
   fastify.route({
     method: "POST",
     url: "/configuration/update",
