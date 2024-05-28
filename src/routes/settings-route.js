a50483839d4e5b63849597f6963ea3b9f6d1272d
mvrzan <mvrzan@icloud.com>
2024-05-27 21:52:42 -0400

    create a new route for database updates

--- src/routes/settings-route.js
+++ src/routes/settings-route.js
@@ -1,10 +1,17 @@
 import updateSettings from "../controllers/update-settings.js";
+import updateUserInfo from "../controllers/update-user-info.js";
 
 const settingsRoute = (fastify, _options, done) => {
   fastify.route({
     method: "POST",
     url: "/configuration",
     handler: updateSettings,
+  });
+
+  fastify.route({
+    method: "POST",
+    url: "/configuration/update",
+    handler: updateUserInfo,
   });
 
   done();
