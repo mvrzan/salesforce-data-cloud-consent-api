a9e215e39c00b63b6a9efb94a2b399bb9fcfb484
mvrzan <mvrzan@icloud.com>
2024-05-28 09:11:05 -0400

    remove qs library and check db values

--- src/utils/server/auth-token.js
+++ src/utils/server/auth-token.js
@@ -1,17 +1,30 @@
-import qs from "qs";
 import axios from "axios";
 import process from "process";
+import { getUserSettings } from "../../database/user-settings.js";
 
 const authToken = async () => {
-  const data = qs.stringify({
+  const settings = await getUserSettings();
+
+  const username =
+    settings.data[0].salesforce_service_username ||
+    process.env.SERVICE_USER_USERNAME;
+  const password =
+    settings.data[0].salesforce_service_password ||
+    process.env.SERVICE_USER_PASSWORD;
+  const securityToken =
+    settings.data[0].salesforce_security_token ||
+    process.env.SERVICE_USER_SECURITY_TOKEN;
+  const clientId = settings.data[0].client_id || process.env.CLIENT_ID;
+  const clientSecret =
+    settings.data[0].client_secret || process.env.CLIENT_SECRET;
+
+  const data = new URLSearchParams({
     grant_type: "password",
-    username: process.env.SERVICE_USER_USERNAME,
-    password:
-      process.env.SERVICE_USER_PASSWORD +
-      process.env.SERVICE_USER_SECURITY_TOKEN,
-    client_id: process.env.CLIENT_ID,
-    client_secret: process.env.CLIENT_SECRET,
-  });
+    username,
+    password: password + securityToken,
+    client_id: clientId,
+    client_secret: clientSecret,
+  }).toString();
 
   const config = {
     method: "post",
