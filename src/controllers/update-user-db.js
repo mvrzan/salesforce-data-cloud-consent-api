81408eeac61acd9f9b81d98ce4c926f059345ed1
mvrzan <mvrzan@icloud.com>
2024-05-28 09:33:39 -0400

    create a controller for user settings operations

---
+++ src/controllers/update-user-db.js
@@ -0,0 +1,26 @@
+import {
+  createUserSettingsTable,
+  updateUserSettings,
+} from "../database/user-settings.js";
+
+const updateUserDb = async (request, reply) => {
+  console.log("Connecting to database...");
+
+  try {
+    // create a new table in the database if one does not exist
+    createUserSettingsTable();
+
+    // insert data into the table
+    updateUserSettings(request.body);
+
+    return reply.status(200).send({
+      message: "User settings updated successfully!",
+    });
+  } catch (error) {
+    console.error(error);
+
+    reply.status(500).send(error);
+  }
+};
+
+export default updateUserDb;
