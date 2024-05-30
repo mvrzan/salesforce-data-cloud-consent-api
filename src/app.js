13f04327c7387e2e818f8381aa5fe7d2c81bf5fd
mvrzan <mvrzan@icloud.com>
2024-05-30 14:30:08 -0400

    updating fastify config

--- src/app.js
+++ src/app.js
@@ -22,6 +22,9 @@ const start = () => {
   try {
     fastify.listen({ port: process.env.PORT || 3000, host: "0.0.0.0" }, () => {
       fastify.log.info(`Server listening on ${fastify.server.address().port}`);
+
+      // console log all the available routes
+      fastify.log.info(fastify.printRoutes());
     });
   } catch (error) {
     fastify.log.error(error);
