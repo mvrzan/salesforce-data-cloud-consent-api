782a7d5b79a1ed5268b56a953a9868d7eabaf392
mvrzan <mvrzan@icloud.com>
2026-01-12 16:24:07 -0500

    refactor processing route to use getProcessing controller

--- server/src/routes/consent-routes.ts
+++ server/src/routes/consent-routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { validateSignature } from "../middleware/validateSignature.ts";
+import getProcessing from "../controllers/getProcessing.ts";
 
 const consentRoutes = Router();
 
-consentRoutes.get("/api/v1/processing/:id", validateSignature, () => {});
+consentRoutes.get("/api/v1/processing/:id", getProcessing);
 consentRoutes.patch("/api/v1/processing/:id", validateSignature, () => {});
 consentRoutes.get("/api/v1/portability/:id", validateSignature, () => {});
 consentRoutes.patch("/api/v1/portability/:id", validateSignature, () => {});
