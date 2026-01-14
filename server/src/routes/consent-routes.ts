7609e1d0856ee7060c6daa1537edb301990309a8
mvrzan <mvrzan@icloud.com>
2026-01-14 11:54:51 -0500

    update patchProcessing route to use the correct controller for handling consent updates

--- server/src/routes/consent-routes.ts
+++ server/src/routes/consent-routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { validateSignature } from "../middleware/validateSignature.ts";
 import getProcessing from "../controllers/getProcessing.ts";
+import patchProcessing from "../controllers/patchProcessing.ts";
 
 const consentRoutes = Router();
 
 consentRoutes.get("/api/v1/processing/:id", getProcessing);
-consentRoutes.patch("/api/v1/processing/:id", validateSignature, () => {});
+consentRoutes.patch("/api/v1/processing/:id", patchProcessing);
 consentRoutes.get("/api/v1/portability/:id", validateSignature, () => {});
 consentRoutes.patch("/api/v1/portability/:id", validateSignature, () => {});
 consentRoutes.get("/api/v1/shouldForget/:id", validateSignature, () => {});
