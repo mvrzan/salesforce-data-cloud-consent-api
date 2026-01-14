8c7d45018dc57607ea499f8119f55d41a4bb5de1
mvrzan <mvrzan@icloud.com>
2026-01-14 12:08:20 -0500

    update shouldForget route to use getPortability controller for handling requests

--- server/src/routes/consent-routes.ts
+++ server/src/routes/consent-routes.ts
@@ -4,12 +4,13 @@ import getProcessing from "../controllers/getProcessing.ts";
 import patchProcessing from "../controllers/patchProcessing.ts";
 import getShouldForget from "../controllers/getShouldForget.ts";
 import patchShouldForget from "../controllers/patchShouldForget.ts";
+import getPortability from "../controllers/getPortability.ts";
 
 const consentRoutes = Router();
 
 consentRoutes.get("/api/v1/processing/:id", getProcessing);
 consentRoutes.patch("/api/v1/processing/:id", patchProcessing);
-consentRoutes.get("/api/v1/portability/:id", validateSignature, () => {});
+consentRoutes.get("/api/v1/portability/:id", getPortability);
 consentRoutes.patch("/api/v1/portability/:id", validateSignature, () => {});
 consentRoutes.get("/api/v1/shouldForget/:id", getShouldForget);
 consentRoutes.patch("/api/v1/shouldForget/:id", patchShouldForget);
